24397774587a8fe98b6889fdfe026236a61eb94f
Marsel633 <akparov1m@gmail.com>
2024-01-14 01:43:30 +0600

    added modal to filter categories

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,38 @@
 import React from 'react';
-import { Stack } from 'expo-router';
+import { Stack, useNavigation } from 'expo-router';
 import {BottomSheetModalProvider} from '@gorhom/bottom-sheet';
 import {CustomHeader} from '@/Components/CustomHeader';
+import Colors from '@/constants/Colors';
+import { TouchableOpacity } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 export const unstable_settings = {
   initialRouteName: 'index',
 };
 
 export default function RootLayoutNav() {
-
+const navigation = useNavigation();
   return (
     <BottomSheetModalProvider>
       <Stack>
         <Stack.Screen name="index" options={{
           header: () => <CustomHeader/>
         }} />
+      <Stack.Screen name='(modal)/filter' options={{
+        presentation: 'modal',
+        headerTitle: 'Filter',
+        headerShadowVisible: false,
+        headerStyle: {
+          backgroundColor: Colors.lightGrey
+        },
+        headerLeft: () => (
+          <TouchableOpacity onPress={() => {
+            navigation.goBack()
+          }}>
+            <Ionicons name='close-outline' size={28} color={Colors.primary}/>
+          </TouchableOpacity>
+        )
+      }}/>
       </Stack>
     </BottomSheetModalProvider>
   );
