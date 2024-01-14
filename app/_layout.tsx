4590c2ebf88fa83b48fb85278382049cbbba3410
Marsel633 <akparov1m@gmail.com>
2024-01-14 18:55:14 +0600

    created search location modal

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -33,6 +33,18 @@ const navigation = useNavigation();
           </TouchableOpacity>
         )
       }}/>
+      <Stack.Screen name='(modal)/location-search' options={{
+        presentation: 'fullScreenModal',
+        headerTitle: 'Select location',
+        headerShadowVisible: false,
+        headerLeft: () => (
+          <TouchableOpacity onPress={() => {
+            navigation.goBack()
+          }}>
+            <Ionicons name='close-outline' size={28} color={Colors.primary}/>
+          </TouchableOpacity>
+        )
+      }}/>
       </Stack>
     </BottomSheetModalProvider>
   );
